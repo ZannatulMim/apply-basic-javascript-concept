f6985d897e3f06e8eb113114286389f2271e8998
ZannatulMim <zannatulmim980@gmail.com>
2023-12-07 21:54:51 +0600

    added header

---
+++ array5.js
@@ -0,0 +1,72 @@
+// function array(numbers) {
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         const index = i;
+//         const element = numbers[index];
+//         sum = sum + element;
+//         console.log(index, element, sum);
+//     }
+// }
+
+// const mynumber = [1, 2, 3, 4, 5, 6];
+// array(mynumber);
+
+
+// function array(numbers) {
+//     sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         const index = i;
+//         const element = numbers[index];
+//         sum = sum + element;
+//         console.log(index, element, sum);
+//     }
+// }
+
+// const mynumber = [1, 2, 3, 4, 5, 6, 7, 8];
+// array(mynumber);
+
+
+// function array(numbers){
+//     let sum=0;
+//     for(let i=0; i<numbers.length; i++){
+//         const index=i;
+//         const element=numbers[index];
+//         sum=sum+element;
+//         console.log(index,element,sum);
+
+//     }
+// }
+
+//     const mynumbers=[1,2,3,4,5,6,7,8];
+//     array(mynumbers);
+
+
+
+// function array(numbers) {
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         const index = i;
+//         const element = numbers[index];
+//         sum = sum + element;
+//         console.log(index, element, sum);
+
+//     }
+// }
+
+// const mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// array(mynumbers);
+
+
+function array(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        sum = sum + element;
+        console.log(index, element, sum)
+    }
+    return sum;
+}
+
+const mynumber = [1, 2, 3, 4, 5, 6];
+array(mynumber);
---
+++ evenarray.js
@@ -0,0 +1,13 @@
+function evennumbers(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        if (element % 2 === 0) {
+            console.log(index, elemrnt);
+        }
+    }
+
+}
+
+const myNumbers = [12, 4, 5, 6, 7, 4, 3, 5];
+evennumbers(myNumbers);
