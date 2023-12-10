c655a0dd58b9efdc6711d30582570776fbaeaba1
ZannatulMim <zannatulmim980@gmail.com>
2023-12-10 23:05:12 +0600

    added

--- evenarray.js
+++ evenarray.js
@@ -1,13 +1,53 @@
-function evennumbers(numbers) {
+// function evennumbers(numbers) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         const index = i;
+//         const element = numbers[index];
+//         if (element % 2 === 0) {
+//             console.log(index, elemrnt);
+//         }
+//     }
+
+// }
+
+
+
+
+// function evennumber(numbers) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         const index = i;
+//         const element = numbers[index];
+//         if (element % 2 === 0) {
+//             console.log(index, element);
+//         }
+//     }
+// }
+
+
+const myNumbers = [12, 4, 5, 6, 7, 4, 3, 5];
+evennumber(myNumbers);
+
+
+
+// function evennumber(numbers) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         const index = i;
+//         const element = numbers[index];
+//         if (element % 2 === 0) {
+//             console.log(index, element);
+//         }
+//     }
+// }
+
+
+
+
+
+function evennumber(numbers) {
     for (let i = 0; i < numbers.length; i++) {
         const index = i;
         const element = numbers[index];
-        if (element % 2 === 0) {
-            console.log(index, elemrnt);
+        if (element % 2 == 0) {
+            console.log(index, element);
         }
     }
-
 }
-
-const myNumbers = [12, 4, 5, 6, 7, 4, 3, 5];
-evennumbers(myNumbers);
---
+++ string.js
@@ -0,0 +1,5 @@
+let name = "bahubrihi";
+for (var i = 0; i <= 9; i++) {
+    console.log(name[i]);
+    console.log(i);
+}
